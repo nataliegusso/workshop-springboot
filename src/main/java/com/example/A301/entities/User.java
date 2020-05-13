bcf5c0a219cebe1860338619c5715baef7f9549e
nataliegusso <nataliegusso@gmail.com>
2020-05-13 15:44:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/A301/entities/User.java
+++ src/main/java/com/example/A301/entities/User.java
@@ -2,10 +2,18 @@ package com.example.A301.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity   //Mapear o JPA, as anotações auxiliam o JPA  //@Entity:
 public class User implements Serializable {  //serializable: transformado em cadeias de bits
 
 	private static final long serialVersionUID = 1L;
 
+	@Id  //Mapear o JPA: Chave primária da tabela do BD
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //Autoimplementável
 	private Long id;
 	private String name;
 	private String email;
