89f3bf95ed6eb866cece4ff384336c0b035993a4
nataliegusso <nataliegusso@gmail.com>
2020-05-18 10:52:13 -0300

    Exception handling - update

--- src/main/java/com/example/A301/services/UserService.java
+++ src/main/java/com/example/A301/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.A301.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +49,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);  //getOne: instancia um usuário, mas não vai no BD, só é monitorado no JPA p uma operação posterior. É mais eficiente.
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);  //getOne: instancia um usuário, mas não vai no BD, só é monitorado no JPA p uma operação posterior. É mais eficiente.
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
